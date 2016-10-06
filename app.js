fccb0210fb5e952c1232247e24a41a9d88e576b5
Janine <janineritchie12@gmail.com>
2016-10-06 11:06:54 +0200

    added changes

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-
 //find the body tag and store it in a variable called 'body'
 var body = document.querySelector("body");
 
 var taxiPositionCounter = 1;
 
-body.onkeydown = function(e){
+body.onkeydown = function(e) {
 
-    //right arrow : 39
+    //right arrow : 39//
     //left arrow  : 37
     //up arrow    : 38
     //down arrow  : 40
+    if (e.keyCode === 39) {
 
+      var blah = calculateTaxiLocation(taxiPositionCounter);
+      taxiPositionCounter++;
+      var blah2 = calculateTaxiLocation(taxiPositionCounter);
+      moveTaxi(blah, blah2);
+
+    }
+    if (e.keyCode === 37) {
+
+      var blah = calculateTaxiLocation(taxiPositionCounter);
+      taxiPositionCounter--;
+      var blah2 = calculateTaxiLocation(taxiPositionCounter);
+      moveTaxi(blah, blah2);
+
+    }
     displayMessage(e.keyCode);
 
+
+
+    //alert(taxiPositionCounter);
     // to move the taxi forward...
     // when the right arrow is pressed
     //work with `taxiPositionCounter`
@@ -23,10 +40,22 @@ body.onkeydown = function(e){
 
     //pass both to the `moveTaxi( firstClassName, secondClassName)` function
     //
-
-    var tl = new TrafficLight(2);
-    
-    tl.orange();
+    var tl = new TrafficLight(taxiPositionCounter);
 
 
-};
+
+          if (e.keyCode === 38) {
+        tl.red();
+      }
+      else if (e.keyCode === 40) {
+        tl.green();
+        displayMessage("green");
+      }
+      else{tl.orange();}
+
+      if (tl.state() === "green") {
+      moveTaxi(blah,blah2);}
+  else if (tl.state()==="red"){taxiPositionCounter=moveTaxi(blah,blah);}
+  else{moveTaxi(blah,blah2)}
+
+}
